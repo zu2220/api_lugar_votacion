e070281379401f3164523e5cc2f1b0d6fd42b902
Oscar Isaac Zari López <151417256+zu2220@users.noreply.github.com>
2025-11-15 20:52:13 -0500

    Change uvicorn host to '0.0.0.0'va

--- main.py
+++ main.py
@@ -85,4 +85,4 @@ def consulta(req: ConsultaRequest):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
